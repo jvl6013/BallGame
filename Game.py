2740ae212f5ed033ced205ba14ae65f0d39f70f4
jvl6013 <jvl6013@psu.edu>
2017-11-07 12:28:59 -0500

    v0.2.1
    Updated ballgame code

--- Game.py
+++ Game.py
@@ -1,11 +1,17 @@
+#Basic setup
 import pygame
+import math
+
 pygame.init()
-ferahgasdgswergaefaw
+
+#RGB
 BLACK = [0, 0, 0]
 
+#Variable
 SCREEN_WIDTH = 600
 SCREEN_HEIGHT = 600
 WINDOW_SIZE = [SCREEN_WIDTH, SCREEN_HEIGHT]
+
 screen = pygame.display.set_mode(WINDOW_SIZE)
 
 done = False
@@ -17,26 +23,76 @@ characterImg = pygame.image.load('macman.png')
 def character(x, y, image):
     screen.blit(image, (x, y))
 
-x = SCREEN_WIDTH/2
-y = SCREEN_HEIGHT/2
+right = False
+left = False
+jump = False
+down = False
 
-up = y = -10
-down = 10
+x = SCREEN_WIDTH/3
+y = SCREEN_HEIGHT - 20
+
+gravity = 1
+jumpHeight = 50
+time = math.sqrt(jumpHeight * 2)
+
+ #Background
+bkgd = pygame.image.load("background.png").convert()
+bkgd_x = 0
 
 while not done:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
 
-    #KEYBOARD INPUT
-    if event.type == pygame.KEYUP:
-        if event.key == pygame.K_SPACE:
-            if(y > 240):
-                y -= 10
+    # KEYBOARD INPUT
+        if event.type == pygame.KEYDOWN:
+            '''            
+            if event.key == pygame.K_RIGHT:
+                print("on")
+                right = True;
+            if event.key == pygame.K_LEFT:
+                left = True;
+            '''
+            if event.key == pygame.K_SPACE:
+                print("on")
+                jump = True;
+
+        if event.type == pygame.KEYUP:
+            '''
+            if event.key == pygame.K_RIGHT:
+                print("off")
+                right = False
+            if event.key == pygame.K_LEFT:
+                left = False
+            '''
+
+    if right:
+        x += 5
+    if left:
+        x -= 5
+
+
+    if jump:
+        if(y > 500 and down is False):
+            y -= 7
+        elif(y < 580 and down is True):
+            print("DOwn")
+            y += 7
+            if(y == 580):
+                jump = False
+                down = False
+        else:
+            down = True
+
+        print(down)
+
+    print(y)
 
 
 
-    screen.fill(BLACK)
+    screen.blit(bkgd, (bkgd_x, 0) )
+
+    #screen.fill(BLACK)
 
     character(x, y, characterImg)
 
